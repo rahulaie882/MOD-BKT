5fd0a11a6aa73faaf907aad509159e47c1fbf4cd
rahulaie882 <rahulr79469@gmail.com>
2026-09-22 23:32:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const ADMIN_ID = "8963867689";
 app.use(express.json());
 
 const bot = new TelegramBot(MASTER_TOKEN, { webHook: true });
-const URL = 'https://mod-bkt-production.up.railway.app';
+const URL = 'https://babaseller.one';
+
 
 // Webhook set karein
 bot.setWebHook(`${URL}/bot${MASTER_TOKEN}`);
